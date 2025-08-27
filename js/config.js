11803d2100243e3ab2d6a8054f879da28b7c378d
Facundosalto <fdsalto@hotmail.com>
2025-08-27 08:32:43 -0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// Credenciales Supabase (tuyas)
+window.SUPABASE_URL = "https://atmcrlvaqmfulmujftrd.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0bWNybHZhcW1mdWxtdWpmdHJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyNDc5NjgsImV4cCI6MjA3MTgyMzk2OH0.ib1A6TlmEoqgPNnOdRh6ZQ0m_3dYrqAxSxCsr6yNoeM";
+
+// Cliente Supabase
+window.sb = window.supabase.createClient(window.SUPABASE_URL, window.SUPABASE_ANON_KEY);
